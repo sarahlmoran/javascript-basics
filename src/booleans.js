3dbc964de6987b551350bc070b1f9dbcbea04b5f
Sarah Moran <s.l.a.moran@gmail.com>
2022-11-29 19:55:39 +0000

    completed 12 boolean tests - 3 remaining

--- src/booleans.js
+++ src/booleans.js
@@ -1,49 +1,49 @@
 function negate(a) {
-  // your code here
+  return !a;
 };
 
 function both(a, b) {
-  // your code here
+  return a&&b;
 };
 
 function either(a, b) {
-  // your code here
+  return a||b;
 };
 
 function none(a, b) {
-  // your code here
+  return !a&&!b;
 };
 
 function one(a, b) {
-  // your code here
+  return a!=b;
 };
 
 function truthiness(a) {
-  // your code here
+ return Boolean(a);
 };
 
 function isEqual(a, b) {
-  // your code here
+  return a===b;
 };
 
 function isGreaterThan(a, b) {
-  // your code here
+  return a>b;
 };
 
 function isLessThanOrEqualTo(a, b) {
-  // your code here
+  return a<=b;
 };
 
 function isOdd(a) {
-  // your code here
+  return a%2===1;
 };
 
 function isEven(a) {
-  // your code here
+  return a%2===0;
 };
 
 function isSquare(a) {
-  // your code here
+  return Math.sqrt(a)%1===0;
 };
 
 function startsWith(char, string) {
